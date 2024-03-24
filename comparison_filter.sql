eaf5fb2aee751365f7de70a68f7218ea0a699616
Theo Obadiah Teguh <theo1329@students.cs.ubc.ca>
2024-03-23 20:15:15 -0700

    Moved non essential DDL temporarily to comparison_filter

--- comparison_filter.sql
+++ comparison_filter.sql
@@ -1,3 +1,28 @@
+-- Import ComparisonData with a Python Jupyter Notebook Script and update the columns and datatypes
+ALTER TABLE ComparisonData
+	CHANGE issue_id Issue_Id INT,
+	CHANGE crsp_cl_grp crsp_cl_grp INT,
+	CHANGE fundid Fund_Id VARCHAR(20),
+	CHANGE quarter _Quarter INT,
+	CHANGE month _Month INT,
+	CHANGE cusip CUSIP VARCHAR(20),
+	CHANGE mktval MarketValue BIGINT,
+	CHANGE shares NumberOfShare BIGINT,
+	CHANGE portdate _Date CHAR(19),
+	CHANGE sign Sign INT,
+	CHANGE short_long_ratio Short_Long_Ratio FLOAT(64,10),
+	CHANGE totmktval_long TotalMarketValueLong BIGINT,
+	CHANGE totmktval TotalMarketValue BIGINT,
+	CHANGE bond_type BondType CHAR(4),
+	CHANGE corp_bond CorpBond INT,
+	CHANGE wght Weighting FLOAT(64,10),
+	CHANGE wght_corp Weighting_Corp FLOAT(64,10);
+
+-- Change Date column datatype after all tables are ready
+UPDATE ComparisonData
+SET _Date = DATE_FORMAT(_Date, '%Y-%m-%d');
+
+-- Some stuff
 CREATE TABLE IF NOT EXISTS HDX_Comparison (
 Fund_Name VARCHAR(40),
 _Date DATE,
@@ -15,7 +40,6 @@ NumberOfShare BIGINT
 
 INSERT INTO HDX_Comparison (Fund_Name, _Date, CUSIP, NumberOfShare, Ticker)
 SELECT Fund_Name, _Date, CUSIP, NumberOfShare, Ticker FROM HoldingDetailX WHERE _Date < '20200930';
-
 
 INSERT INTO Checker_Comparison (crsp_cl_grp, _Date, CUSIP, NumberOfShare)
 SELECT crsp_cl_grp, _Date, CUSIP, NumberOfShare FROM ComparisonData;
--- setup.sql
+++ setup.sql
@@ -85,37 +85,9 @@ INTO TABLE HoldingDetailX
 FIELDS TERMINATED BY ';'  
 LINES TERMINATED BY '\n';
 
-/*
--- Import ComparisonData with a Python Jupyter Notebook Script and update the columns and datatypes
-
-ALTER TABLE ComparisonData
-	CHANGE issue_id Issue_Id INT,
-	CHANGE crsp_cl_grp crsp_cl_grp INT,
-	CHANGE fundid Fund_Id VARCHAR(20),
-	CHANGE quarter _Quarter INT,
-	CHANGE month _Month INT,
-	CHANGE cusip CUSIP VARCHAR(20),
-	CHANGE mktval MarketValue BIGINT,
-	CHANGE shares NumberOfShare BIGINT,
-	CHANGE portdate _Date CHAR(19),
-	CHANGE sign Sign INT,
-	CHANGE short_long_ratio Short_Long_Ratio FLOAT(64,10),
-	CHANGE totmktval_long TotalMarketValueLong BIGINT,
-	CHANGE totmktval TotalMarketValue BIGINT,
-	CHANGE bond_type BondType CHAR(4),
-	CHANGE corp_bond CorpBond INT,
-	CHANGE wght Weighting FLOAT(64,10),
-	CHANGE wght_corp Weighting_Corp FLOAT(64,10);
-
--- Change Date column datatype after all tables are ready
-
+-- Change _Date columns' datatype after all tables are ready
 UPDATE PortfolioSummaryX 
 SET _Date = STR_TO_DATE(_Date, '%Y-%m-%d');
 
 UPDATE HoldingDetailX 
 SET _Date = STR_TO_DATE(_Date, '%Y-%m-%d');
-
-UPDATE ComparisonData
-SET _Date = DATE_FORMAT(_Date, '%Y-%m-%d');
-
--- Continue to filtration script */
